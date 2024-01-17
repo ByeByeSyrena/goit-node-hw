ccf31178c6675c48f11cd6bac8a588f71f046d4d
Ira S <iryna.shevchenko2@gmail.com>
2024-01-17 17:14:10 +0200

    done

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,23 +1,19 @@
 const { dataValidator, httpError, ctrlWrapper } = require("../helpers");
 
 const validateFields = async (req, res, next) => {
-  try {
-    const { body } = req;
+  const { body } = req;
 
-    if (Object.keys(req.body).length === 0) {
-      httpError(400, "missing fields");
-    }
-
-    const { error } = await dataValidator(body);
-
-    if (error) {
-      httpError(400, error.message);
-    }
-
-    next();
-  } catch (err) {
-    console.error(err);
+  if (Object.keys(req.body).length === 0) {
+    httpError(400, "missing fields");
   }
+
+  const { error } = await dataValidator(body);
+
+  if (error) {
+    httpError(400, error.message);
+  }
+
+  next();
 };
 
 module.exports = ctrlWrapper(validateFields);
