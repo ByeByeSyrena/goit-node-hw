ec2f9690f0a608dae2f56ed51d80f93e223f0f86
Ira S <iryna.shevchenko2@gmail.com>
2024-01-17 21:50:44 +0200

    in pr

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,6 @@
 const validateFields = require("./validateFields");
 const isValidId = require("./isValidId");
 const handleMongooseError = require("./handleMongooseError");
-const checkToken = require("./authentification");
+const { checkToken } = require("./auth");
 
 module.exports = { validateFields, isValidId, handleMongooseError, checkToken };
